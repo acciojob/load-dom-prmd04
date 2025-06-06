1fb1a845ec9a9b465c47e1dbd7fea94eb002fd42
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-06 19:23:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+window.addEventListener("load", ()=>{
+	let p = document.createElement("p");
+	p.innerText = "DOM load success";
+	document.body.appendChild(p);
+})
